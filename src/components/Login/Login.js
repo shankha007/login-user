68ec446fc3e1c9542ef74bdceacaeabf9a5c745c
Shankha <ribhumaster@gmail.com>
2021-08-16 11:52:06 +0530

    optimized login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifer = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    
+    return () => {
+      clearTimeout(identifer);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
